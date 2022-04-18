0593a8c8f020da9e775bd5ee6c709e76d7e243a6
Tommykondraros <100490021+Tommykondraros@users.noreply.github.com>
2022-04-18 17:35:49 -0500

    Add files via upload

---
+++ assignment 3/assignment 3 q3.py
@@ -0,0 +1,11 @@
+books=int(input("number of books"))
+cost=float(input("cost per book"))
+total=books*cost
+if(total>50):
+  setattrhipping=0
+else:
+  shipping=25
+print("total: ")
+print(total)
+print("shipping charge: ")
+print(shipping)
